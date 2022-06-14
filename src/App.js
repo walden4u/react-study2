40858a2aee6dab0880cf3ff47914bada392394f9
Alex Kwon <walden4u@gmail.com>
2022-06-14 05:39:45 +0000

    ch2_4_5 undefined를 렌더링하지 않기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 function App() {
   const name = '리액트';
+  const name2 = undefined;
   return (
     <>
       <h1>Hello {name}</h1>
@@ -9,6 +10,9 @@ function App() {
       {name === '리액트' ? <h2>리액트 입니다!</h2> : <h2>리액트가 아닙니다!</h2>}
       {name === '리액트' ? <h2>리액트 입니다!</h2> : null}  
       {name === '리액트' && <h2>리액트 입니다!</h2>}
+
+      <h2>{name2}</h2>
+      <h2>{name2 || '리액트'}</h2>
     </>
   );
 }

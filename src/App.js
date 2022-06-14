2ee0c5c93adbbba8539b833bcf221c88a9d1eebe
Alex Kwon <walden4u@gmail.com>
2022-06-14 03:13:38 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <h2>How are you?</h2>
+    </>
+  );
+}
+
+export default App;

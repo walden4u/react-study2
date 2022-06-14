c62c81f563710413eac944a8129cd339c844e797
Alex Kwon <walden4u@gmail.com>
2022-06-14 21:30:11 +0000

    ch2_5 ESLint와 Prettier 적용하기

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,31 +1,35 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name = '리액트';
+  const name = "리액트";
   const name2 = undefined;
   const style = {
     // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px', // font-size -> fontSize
-    fontWeight: 'bold', // font-weight -> fontWeight
-    padding: 16 // 단위를 생략하면 px로 지정됩니다.
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px", // font-size -> fontSize
+    fontWeight: "bold", // font-weight -> fontWeight
+    padding: 16, // 단위를 생략하면 px로 지정됩니다.
   };
-  
+
   return (
     <>
       <h1 style={style}>Hello!!! {name}</h1>
       <h2 className="react">How are you?</h2>
-      <br />    {/* 한줄 주석입니다. */}  
-      <input />   {/* 여러줄 
+      <br /> {/* 한줄 주석입니다. */}
+      <input />{" "}
+      {/* 여러줄 
       
-      주석입니다. */} 
-      {name === '리액트' ? <h2>리액트 입니다!</h2> : <h2>리액트가 아닙니다!</h2>}
-      {name === '리액트' ? <h2>리액트 입니다!</h2> : null}  
-      {name === '리액트' && <h2>리액트 입니다!</h2>}
-
+      주석입니다. */}
+      {name === "리액트" ? (
+        <h2>리액트 입니다!</h2>
+      ) : (
+        <h2>리액트가 아닙니다!</h2>
+      )}
+      {name === "리액트" ? <h2>리액트 입니다!</h2> : null}
+      {name === "리액트" && <h2>리액트 입니다!</h2>}
       <h2>{name2}</h2>
-      <h2>{name2 || '리액트'}</h2>
+      <h2>{name2 || "리액트"}</h2>
     </>
   );
 }

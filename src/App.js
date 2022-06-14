1d8937531a423d44f8ae17585c13c65d98c44b78
Alex Kwon <walden4u@gmail.com>
2022-06-14 05:33:42 +0000

    ch2_4_3 If문 대신 조건부 연산자

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
     <>
-      <h1>Hello React!</h1>
+      <h1>Hello {name}</h1>
       <h2>How are you?</h2>
+      {name === '리액트' ? <h2>리액트 입니다!</h2> : <h2>리액트가 아닙니다!</h2>}
+      {name === '리액트' ? <h2>리액트 입니다!</h2> : null}  
+      {name === '리액트' && <h2>리액트 입니다!</h2>}
     </>
   );
 }

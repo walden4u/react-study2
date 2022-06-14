f521c820bb783ff131c3cb335f8267f749ff9729
Alex Kwon <walden4u@gmail.com>
2022-06-14 12:18:50 +0000

    ch2_4_6 인라인 스타일링

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -14,7 +14,7 @@ function App() {
   
   return (
     <>
-      <h1>Hello {name}</h1>
+      <h1 style={style}>Hello!!! {name}</h1>
       <h2>How are you?</h2>
       {name === '리액트' ? <h2>리액트 입니다!</h2> : <h2>리액트가 아닙니다!</h2>}
       {name === '리액트' ? <h2>리액트 입니다!</h2> : null}  
